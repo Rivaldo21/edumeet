c4ed1bd33f7dc5b7af347c3503845876009635a3
Ashok_Spundan <ashok.kushwaha@in.spundan.com>
2024-08-08 11:38:09 +0530

    fixed the meeting joining Error

--- README.md
+++ README.md
@@ -1,24 +1,43 @@
 # edumeet
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## back-end server
+
+start the backend server
+
+```
+npm start
+```
+
+hit the APi through
+
+```
+http://localhost:3000/
+```
--- src/components/JitsiMeet.vue
+++ src/components/JitsiMeet.vue
@@ -8,22 +8,24 @@ export default {
   mounted() {
     const domain = "8x8.vc";
     const options = {
-      roomName: "vpaas-magic-cookie-a60420f14af34bceba2584ddb6390b51/SampleAppInterestedSurplusTerrifyThen",
-      parentNode: document.querySelector('#jaas-container'),
-      jwt: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InZwYWFzLW1hZ2ljLWNvb2tpZS1hNjA0MjBmMTRhZjM0YmNlYmEyNTg0ZGRiNjM5MGI1MS83MTMxZGQifQ.eyJhdWQiOiJqaXRzaSIsImlzcyI6ImNoYXQiLCJpYXQiOjE3MjIxMzI0MjgsImV4cCI6NDg3NTczMjQyOCwibmJmIjoxNzIyMTMyNDI4LCJzdWIiOiJ2cGFhcy1tYWdpYy1jb29raWUtYTYwNDIwZjE0YWYzNGJjZWJhMjU4NGRkYjYzOTBiNTEiLCJjb250ZXh0Ijp7ImZlYXR1cmVzIjp7ImxpdmVzdHJlYW1pbmciOnRydWUsIm91dGJvdW5kLWNhbGwiOnRydWUsInNpcC1vdXRib3VuZC1jYWxsIjpmYWxzZSwidHJhbnNjcmlwdGlvbiI6dHJ1ZSwicmVjb3JkaW5nIjp0cnVlfSwidXNlciI6eyJoaWRkZW4tZnJvbS1yZWNvcmRlciI6ZmFsc2UsIm1vZGVyYXRvciI6dHJ1ZSwibmFtZSI6IiIsImlkIjoiMzdiZTI2NjItNDAwNC00YTUyLTk2YjMtY2I5NjMwZGM2MjE1IiwiYXZhdGFyIjoiIiwiZW1haWwiOiIifX0sInJvb20iOiIqIn0.X4osyaqy32wE5menDSTtTGVnnsUUpUn_HrkQiJtAkTuo5QukGKNeBS9Fy8joejSCbqqHObPvA5spb4Osf5ACfiEubPK5NiyEw9HvFHM0v_rxPcxixSwXdYqFi_BlmVVQxvg4ATi2luAwf4TIQkuzgMsvKDwp4BAOaYmCyzjxHdwJuNcIzSHwdje4wzVyjIYSSBymxamsoi3bt8ayCcATvzSYrx9mFXz31f2oSBYbUTSO_DjzcyOPWZ5Nv385OAv7X9Dldg5NoIXwZdQ-o4EQR5WNaki9h9T9XVh70o48leMmtJgLdjeGX2_EYNFt_Th65p_BCax_wSdl0VZ4Y9TVHw"
+      roomName:
+        "vpaas-magic-cookie-8bb988617ed14047acc207e7f35ddcaa/SampleAppInterestedSurplusTerrifyThen",
+      parentNode: document.querySelector("#jaas-container"),
+      jwt: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InZwYWFzLW1hZ2ljLWNvb2tpZS04YmI5ODg2MTdlZDE0MDQ3YWNjMjA3ZTdmMzVkZGNhYS8xZDY4NzUifQ.eyJhdWQiOiJqaXRzaSIsImNvbnRleHQiOnsidXNlciI6eyJpZCI6IjBhMGMzNjdjLThhMjItNGZlZS05MzlkLTMxYWFmYTZiN2UwZCIsIm5hbWUiOiJSIEoiLCJhdmF0YXIiOiJhdmF0YXIiLCJlbWFpbCI6InJqMTQyODcyQGdtYWlsLmNvbSIsIm1vZGVyYXRvciI6InRydWUifSwiZmVhdHVyZXMiOnsibGl2ZXN0cmVhbWluZyI6InRydWUiLCJyZWNvcmRpbmciOiJ0cnVlIiwidHJhbnNjcmlwdGlvbiI6InRydWUiLCJvdXRib3VuZC1jYWxsIjoidHJ1ZSJ9fSwiaXNzIjoiY2hhdCIsInJvb20iOiIqIiwic3ViIjoidnBhYXMtbWFnaWMtY29va2llLThiYjk4ODYxN2VkMTQwNDdhY2MyMDdlN2YzNWRkY2FhIiwiZXhwIjoxNzIzMTA3Nzc0LCJuYmYiOjE3MjMwOTY5NjQsImlhdCI6MTcyMzA5Njk3NH0.O3npbakap6_40in0G8bzxxxhUtZRhgfQlhrjfEt6L6z_-Oft6YyPWlkaCSO1nWQy_6FRjoXP8NggrXhaZ9mRQEb9up-75vbEw7Bj96ls9dA04dmHkeFf6UWOY53QbX9NqMY1oIxj1QToyKFrSqkSROgoYl4Ag6e4pLBypHk9yBikxeHDO02EOIXYTRwdJX3nygqKL-TJo5hr29uiAafe8s1yP-tonFEZTD8eCVfIg1MAGF2D-GUrIRYyKGo76bYC2ooHkMqvaoHBYsRAi0yvori6P_UGTdFBOj-4Jx3hb4zhv9NjXI6ibrbaXaErlV4VYXgqqBQmD7b3sMleAZAhnw",
     };
 
     /* global JitsiMeetExternalAPI */
     new JitsiMeetExternalAPI(domain, options);
-  }
+  },
 };
 </script>
 
 <style scoped>
-html, body, #jaas-container {
+html,
+body,
+#jaas-container {
   height: 100%;
   margin: 0;
   padding: 0;
 }
 </style>
-
