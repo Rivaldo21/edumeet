8e6348f575aeae988befdeeaa7062c5dcf49b759
Rivaldo Guterre <rj142872@gmail.com>
2024-08-08 23:38:36 +0900

    New Update 1.7

--- api/generate-jwt.js
+++ api/generate-jwt.js
@@ -74,7 +74,7 @@ app.post('/api/generate-jwt', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
