9095848e56be7e8048ac819b915dadc3fc248de2
Rivaldo Guterre <rj142872@gmail.com>
2024-08-12 11:03:46 +0900

    New Update 5.5

--- api/generate-jwt.js
+++ api/generate-jwt.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Mengambil kunci privat dari variabel lingkungan
 const privateKey = process.env.JWT_PRIVATE_KEY;
-const appId = 'vpaas-magic-cookie-a60420f14af34bceba2584ddb6390b51/room';
+const appId = 'vpaas-magic-cookie-a60420f14af34bceba2584ddb6390b51';
 const keyId = 'vpaas-magic-cookie-a60420f14af34bceba2584ddb6390b51/bcf313';
 
   app.post("/api/generate-jwt", (req, res) => {
@@ -41,7 +41,7 @@ const keyId = 'vpaas-magic-cookie-a60420f14af34bceba2584ddb6390b51/bcf313';
       },
       iss: "chat",
       room: room,
-      sub: appId, // Sesuaikan dengan App ID yang benar
+      sub: appId, 
       exp,
       nbf,
     };
