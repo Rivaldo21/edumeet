7eb9cb0bdd199a19f0dc6f728dfff678ad3c375d
Rivaldo Guterre <rj142872@gmail.com>
2024-08-13 01:04:21 +0900

    New Update 5.7

--- api/generate-jwt.js
+++ api/generate-jwt.js
@@ -3,7 +3,7 @@ const uuid = require("uuid-random");
 const express = require("express");
 
 const app = express();
-const port = 3000; // Declared port
+const port = 3000;
 
 // Middleware untuk parse JSON body
 app.use(express.json()); 
@@ -13,15 +13,10 @@ const privateKey = process.env.JWT_PRIVATE_KEY;
 const appId = 'vpaas-magic-cookie-a60420f14af34bceba2584ddb6390b51';
 const keyId = 'vpaas-magic-cookie-a60420f14af34bceba2584ddb6390b51/bcf313';
 
-app.post("/api/generate-jwt", (req, res) => {
+  app.post("/api/generate-jwt", (req, res) => {
   const { room, name, email, avatar } = req.body;
 
-  // Validate incoming data
-  if (!room || !name || !email) {
-    return res.status(400).json({ error: "Missing required fields" });
-  }
-
-  // Function to generate JWT
+  // Fungsi untuk menghasilkan JWT
   const generate = (privateKey, { id, name, email, avatar, kid }) => {
     const now = Math.floor(Date.now() / 1000);
     const exp = now + (60 * 60);
@@ -30,17 +25,23 @@ app.post("/api/generate-jwt", (req, res) => {
     const payload = {
       aud: "jitsi",
       context: {
-        user: { id, name, avatar, email, moderator: true },
+        user: {
+          id,
+          name,
+          avatar,
+          email,
+          moderator: true,
+        },
         features: {
-          livestreaming: true,
-          recording: true,
-          transcription: true,
-          "outbound-call": true,
+          livestreaming: "true",
+          recording: "true",
+          transcription: "true",
+          "outbound-call": "true",
         },
       },
       iss: "chat",
       room: room,
-      sub: appId,
+      sub: appId, 
       exp,
       nbf,
     };
@@ -51,24 +52,18 @@ app.post("/api/generate-jwt", (req, res) => {
     });
   };
 
-  try {
-    const token = generate(privateKey, {
-      id: uuid(),
-      name,
-      email,
-      avatar: avatar || '',
-      kid: keyId,
-    });
+  const token = generate(privateKey, {
+    id: uuid(),
+    name,
+    email,
+    avatar,
+    kid: keyId,
+  });
 
-    console.log("Generated Token:", token);
-    res.json({ token });
-  } catch (error) {
-    console.error('Error generating token:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+  console.log("Generated Token:", token);
+  res.json({ token }); 
 });
 
-// Start server and listen on the port
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`App listening on port ${port}`);
 });
